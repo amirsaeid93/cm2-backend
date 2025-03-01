e97d947f25a09176bcac267fc23a25154c0acdd8
amirsaeid93 <saeid.taebnia@metropolia.fi>
2025-03-01 20:18:39 +0200

    Update

--- api-server-starter/config/db.js
+++ api-server-starter/config/db.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB Connected...");
+    } catch (error) {
+        console.error("MongoDB Connection Failed", error);
+        process.exit(1); // Exit process with failure
+    }
 };
 
 module.exports = connectDB;
